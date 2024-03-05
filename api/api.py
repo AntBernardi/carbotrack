0548ebf122a577c1edf048809134fe17b342db75
Cyril Marchand <marchand.cyr@gmail.com>
2024-03-05 14:39:48 +0100

    Changes to be committed: 	modified:   api/api.py     Added relative path instead of hardcoded one

--- api/api.py
+++ api/api.py
@@ -1,7 +1,14 @@
 import sys
+import os
 
-sys.path.insert(0, '/home/daliryc/code/AntBernardi/carbotrack')
+# Get the directory of the current file
+current_dir = os.path.dirname(os.path.realpath(__file__))
 
+# Get the parent directory
+parent_dir = os.path.dirname(current_dir)
+
+# Add the parent directory to sys.path
+sys.path.insert(0, parent_dir)
 from fastapi import FastAPI
 from carbotrack_code.interface.function import get_full_result
 
@@ -19,3 +26,10 @@ def predict(image):
     return {'You are eating': food_result,
             'Carbs quantity': carbs_result,
             'Insuline doses recommended': insuline_result}
+
+@app.get('/dummy_test')
+def dummy(int: int):
+    if int == 1:
+        return {'One'}
+    else:
+        return {'Please input 1 as parameter'}    
